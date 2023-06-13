a5a7d82df4669ce230caf470fde02410d51ce7f1
Gustavo Guimarães Martins <90704921+GustavoGuimaraes32@users.noreply.github.com>
2023-06-12 23:01:56 -0300

    Responsividade tela de cadastro

--- frontend/src/app/components/signup-page/signup-page.component.ts
+++ frontend/src/app/components/signup-page/signup-page.component.ts
@@ -12,7 +12,30 @@ export class SignupPageComponent
 
   ngOnInit()
   {
-    CreateAccountPopUp();
+    const createAccount = document.getElementById("create-account-btn");
+    const closePopUpBtn = document.getElementById("close-popup-btn");
+    const popUp = document.getElementsByClassName("popup")[0];
+    const closePopup = document.getElementById("closePopup");
+
+    /* Inputs do form */
+    const Nome = <HTMLInputElement>document.getElementById("Nome");
+    const Email = <HTMLInputElement>document.getElementById("Email");
+    const Senha = <HTMLInputElement>document.getElementById("Senha");
+    const ConfirmSenha = <HTMLInputElement>document.getElementById("ConfirmSenha");
+
+    createAccount?.addEventListener("click", function()
+    {
+      if(Nome?.value !== "" && Email?.value !== "" && Senha?.value !== "" && ConfirmSenha?.value !== "")
+      {
+        popUp.classList.toggle("active");
+      }
+    });
+
+    closePopup?.addEventListener("click", function()
+    {
+      popUp.classList.remove("active");
+    });
+
   }
 
 }
