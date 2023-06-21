98baf619ab792d6f31cbd34cdf05af67c07cbcf6
Leonardo Alves <alvesleo2002@gmail.com>
2023-06-21 20:13:34 -0300

    arruamndo pra apresentar

--- frontend/src/app/pages/event-page/event-page.component.ts
+++ frontend/src/app/pages/event-page/event-page.component.ts
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class EventPageComponent {
 
+  artist: any[] = [
+    {
+      'name' : 'Matue',
+      'photo' : '../../../assets/matue-mock.svg'
+    },
+    {
+      'name' : 'WC no Beat',
+      'photo' : '../../../assets/wcnobeat.svg'
+    },
+    {
+      'name' : 'l7nnon',
+      'photo' : '../../../assets/l7.svg'
+    },
+    {
+      'name' : 'Hyperanhas',
+      'photo' : '../../../assets/hyperanhas.svg'
+    },
+    {
+      'name' : 'Filipe Ret',
+      'photo' : '../../../assets/ret.svg'
+    },
+    {
+      'name' : 'Cabelinho',
+      'photo' : '../../../assets/cabelinho.svg'
+    },
+    {
+      'name' : 'Sidoka',
+      'photo' : '../../../assets/sidoka.svg'
+    },
+    {
+      'name' : 'Orochi',
+      'photo' : '../../../assets/orochi.svg'
+    },
+    {
+      'name' : 'Teto',
+      'photo' : '../../../assets/teto.svg'
+    },
+    {
+      'name' : 'Chefin',
+      'photo' : '../../../assets/chefin.svg'
+    }
+  ]
 }
